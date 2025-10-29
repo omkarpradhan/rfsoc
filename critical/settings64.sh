ea39c606faf7c6ee02819adb83d2717e3be1bde5
pradhan <omkar.pradhan@jpl.nasa.gov>
2025-10-28 17:38:42 -0700

    - updated readme - added settings.sh for model composer which was not autmatically created by installer

---
+++ critical/settings64.sh
@@ -0,0 +1,7 @@
+##############################################################
+# Copyright (c) 1986-2025 Xilinx, Inc.  All rights reserved. #
+##############################################################
+
+source /opt/Xilinx/Vivado/2021.1/.settings64-Vivado.sh
+source /opt/Xilinx/Model_Composer/2021.1/.settings64-Model_Composer.sh
+source /opt/Xilinx/Vitis_HLS/2021.1/.settings64-Vitis_HLS.sh
